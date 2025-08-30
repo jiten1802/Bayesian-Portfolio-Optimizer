a426ed5a7c3a1fcc026a13d7955350e2a1e70bf9
jiten1802 <shahjiten1802@gmail.com>
2025-08-31 01:31:28 +0530

    Update

---
+++ build/CMakeFiles/optimizer.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/optimizer.dir/external/QuadProd++/QuadProg++.cc.obj"
+  "CMakeFiles/optimizer.dir/external/QuadProd++/QuadProg++.cc.obj.d"
+  "CMakeFiles/optimizer.dir/src/main.cpp.obj"
+  "CMakeFiles/optimizer.dir/src/main.cpp.obj.d"
+  "liboptimizer.dll.a"
+  "optimizer.exe"
+  "optimizer.exe.manifest"
+  "optimizer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/optimizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
